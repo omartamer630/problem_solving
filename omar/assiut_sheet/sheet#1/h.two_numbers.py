365585e1fb4213f9eb3f794a5806d2b7083b4902
Omar Tamer <omartamerdev@gmail.com>
2025-07-19 21:51:31 +0300

    fixed the two number

--- omar/assiut_sheet/sheet#1/h.two_numbers.py
+++ omar/assiut_sheet/sheet#1/h.two_numbers.py
@@ -2,17 +2,14 @@ numb1, numb2 = input().split()
 dev = float(numb1) / float(numb2)
 
 
-
 operation_floor = int(numb1) // int(numb2)
 
 subtraction = dev - operation_floor
 
-operation_ceil = int(int(numb1) // int(numb2)) + 1
+operation_ceil = int(int(numb1) // int(numb2)) + 1 if numb1 != numb2 else 1
 
 operation_round = operation_floor + 1 if subtraction >= 0.5 else operation_floor
 
-print(f"floor {numb1} / {numb2} = {operation_floor} ")
-print(f"ceil {numb1} / {numb2} = {operation_ceil} ")
-print(f"round {numb1} / {numb2} = {operation_round} ")
-
-
+print(f"floor {numb1} / {numb2} = {operation_floor}")
+print(f"ceil {numb1} / {numb2} = {operation_ceil}")
+print(f"round {numb1} / {numb2} = {operation_round}")
