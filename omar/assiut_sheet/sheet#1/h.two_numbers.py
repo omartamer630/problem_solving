4190ec605e752ec06a28c913ade223cbcfa18c2b
Omar Tamer <omartamerdev@gmail.com>
2025-07-19 21:45:41 +0300

    fixed two numbers  problem

--- omar/assiut_sheet/sheet#1/h.two_numbers.py
+++ omar/assiut_sheet/sheet#1/h.two_numbers.py
@@ -11,10 +11,6 @@ operation_ceil = int(int(numb1) // int(numb2)) + 1
 
 operation_round = operation_floor + 1 if subtraction >= 0.5 else operation_floor
 
-rounded = (float(numb1) / float(numb2))
-print(rounded)
-operation_round = round(rounded)
-
 print(f"floor {numb1} / {numb2} = {operation_floor} ")
 print(f"ceil {numb1} / {numb2} = {operation_ceil} ")
 print(f"round {numb1} / {numb2} = {operation_round} ")
